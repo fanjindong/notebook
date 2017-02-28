af0fc7861cbbcd753f57eeb9e033c0c8f09f3584
fanjindong <765912710@qq.com>
2017-02-28 16:24:28 +0800

    增加蜘蛛网电影信息代码

--- celery/celery_demo/celery_app/celeryconfig.py
+++ celery/celery_demo/celery_app/celeryconfig.py
@@ -3,13 +3,14 @@ from datetime import timedelta
 from celery.schedules import crontab
 # Broker and Backend
 BROKER_URL = 'pyamqp://guest@localhost//'
-#CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+# CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
 # Timezone
 CELERY_TIMEZONE = 'Asia/Shanghai'    # 指定时区，不指定默认为 'UTC'
 # CELERY_TIMEZONE='UTC'
 # import
 CELERY_IMPORTS = (
-    'celery_app.task1'
+    'celery_app.task1',
+    'celery_app.spider_cinema_film'
 )
 
 CELERY_ANNOTATIONS = {
@@ -22,9 +23,19 @@ CELERY_ANNOTATIONS = {
 CELERYD_TASK_TIME_LIMIT = 60 * 3
 
 CELERYBEAT_SCHEDULE = {
-    'add-every-600-seconds': {
-        'task': 'celery_app.task1.update_hotel',
-        'schedule': timedelta(seconds=60 * 30),       # 每 30 分钟执行一次
+    # 'add-every-600-seconds': {
+    #     'task': 'celery_app.task1.update_hotel',
+    #     'schedule': timedelta(seconds=60 * 30),       # 每 30 分钟执行一次
+    #  'options': {'queue': "schedule"}          # 配置队列
+    # },
+    'add-every-1-day': {
+        'task': 'celery_app.spider_cinema_film.city_id_fetch',
+        'schedule': crontab(hour=19, minute=40),       # 每 天 执行一次
+        #  'options': {'queue': "schedule"}          # 配置队列
+    },
+    'add-every-1-day': {
+        'task': 'celery_app.spider_cinema_film.cinema_id_fetch',
+        'schedule': crontab(hour=19, minute=43),       # 每 天 分钟执行一次
         #  'options': {'queue': "schedule"}          # 配置队列
     }
 }
